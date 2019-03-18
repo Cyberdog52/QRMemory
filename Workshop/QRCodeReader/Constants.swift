767ca8282c29714e1d55a83b17bb36af7d0f3883
Andres Konrad <andres.konrad@hotmail.com>
2019-03-18 18:51:23 +0100

    Remove Todo 2

--- Workshop/QRCodeReader/Constants.swift
+++ Workshop/QRCodeReader/Constants.swift
@@ -16,19 +16,5 @@ class Constants {
     // TODO 2
     // add supportedCodeTypes to the array
     // AVMetadataObject.ObjectType. ...
-    static let supportedCodeTypes = [AVMetadataObject.ObjectType.qr]
-    
-    static let allSupportedCodeTypes = [AVMetadataObject.ObjectType.upce,
-                                     AVMetadataObject.ObjectType.code39,
-                                     AVMetadataObject.ObjectType.code39Mod43,
-                                     AVMetadataObject.ObjectType.code93,
-                                     AVMetadataObject.ObjectType.code128,
-                                     AVMetadataObject.ObjectType.ean8,
-                                     AVMetadataObject.ObjectType.ean13,
-                                     AVMetadataObject.ObjectType.aztec,
-                                     AVMetadataObject.ObjectType.pdf417,
-                                     AVMetadataObject.ObjectType.itf14,
-                                     AVMetadataObject.ObjectType.dataMatrix,
-                                     AVMetadataObject.ObjectType.interleaved2of5,
-                                     AVMetadataObject.ObjectType.qr]
+    static let supportedCodeTypes = [AVMetadataObject.ObjectType]()
 }
